8a8f6a72fbf6218d0e6a409aab411610d0a38795
Pedro Flores <pflores@apttus.com>
2017-11-30 18:36:57 -0800

    feat: Add NewDeck component. Note: it still needs to persist the data to the database

---
+++ components/NewDeck.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { white, black, gray } from '../utils/colors';
+
+export default class NewDeck extends React.Component {
+
+  state = {
+    deckTitle: '',
+  };
+
+  handleTitleTextChange = text => {
+    this.setState({
+      deckTitle: text,
+    });
+  };
+
+  handleSubmit = () => {
+    // TODO: save to local storage
+    this.props.navigation.navigate('Home');
+  };
+
+  render() {
+    const { deckTitle } = this.state;
+
+    return (
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+        <Text style={styles.titleQuestionText}>What is the title of your deck?</Text>
+        <TextInput
+          value={deckTitle}
+          placeholder='Deck title'
+          style={styles.textInput}
+          onChangeText={this.handleTitleTextChange}
+        />
+        <TouchableOpacity
+          style={styles.submitBtn}
+          onPress={this.handleSubmit}
+        >
+          <Text style={styles.btnText}>
+            Submit
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: white,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleQuestionText: {
+    textAlign: 'center',
+    fontSize: 34,
+    padding: 30,
+  },
+  textInput: {
+    width: 300,
+    height: 44,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: gray,
+    marginTop: 30,
+    backgroundColor: white,
+  },
+  submitBtn: {
+    backgroundColor: black,
+    margin: 5,
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 50,
+    borderRadius: 5,
+  },
+  btnText: {
+    color: white,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
