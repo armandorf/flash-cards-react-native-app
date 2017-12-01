858a2be93f01a892e42f78d69e9532634212d82f
Pedro Flores <pflores@apttus.com>
2017-11-30 23:28:37 -0800

    refactor: Rename and format APIs

--- utils/api.js
+++ utils/api.js
@@ -1,21 +1,23 @@
 import { AsyncStorage } from 'react-native';
 
-export function getDecks() {
+export function getAllDecks() {
   return AsyncStorage.getAllKeys()
     .then(deckTitles => AsyncStorage.multiGet(deckTitles, (err, stores) => stores))
     .then(stores => stores.map(result => JSON.parse(result[1])));
 }
 
 export function getDeck(deckTitle) {
-  return AsyncStorage.getItem(deckTitle).then(deck => JSON.parse(deck));
+  return AsyncStorage.getItem(deckTitle)
+    .then(deck => JSON.parse(deck));
 }
 
-export function saveDeckTitle(deckTitle) {
+export function saveDeck(deckTitle) {
   const deck = {
     title: deckTitle,
     questions: [],
   };
-  return AsyncStorage.setItem(deckTitle, JSON.stringify(deck));
+  return AsyncStorage.setItem(deckTitle, JSON.stringify(deck))
+    .then(() => getDeck(deckTitle));
 }
 
 export function addCardToDeck(deckTitle, card) {
