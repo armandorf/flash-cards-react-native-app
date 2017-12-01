e5fcd3bc96121ca681b17c08c43a31ac2e986ca3
Pedro Flores <pflores@apttus.com>
2017-11-30 18:33:10 -0800

    feat: Implement main view with 2 tabs using a TabNavigator and a StackNavigator

--- App.js
+++ App.js
@@ -1,25 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, AsyncStorage } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Constants } from 'expo';
+import DeckList from './components/DeckList';
+import DeckItem from './components/DeckItem';
+import NewDeck from './components/NewDeck';
+import Deck from './components/Deck';
+import Quiz from './components/Quiz';
+import NewCard from './components/NewCard';
+import { purple, white, black, gray } from './utils/colors';
+
+function CardsStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+    },
+  },
+  AddEntry: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: black,
+    style: {
+      height: 56,
+      backgroundColor: white,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    },
+    labelStyle: {
+      color: black,
+      fontSize: 20,
+    },
+  },
+});
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  DeckDetails: {
+    screen: Deck,
+    navigationOptions: {
+      title: 'Deck Details',
+      headerTintColor: black,
+      headerStyle: {
+        backgroundColor: white,
+      },
+    },
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: {
+      title: 'Quiz',
+      headerTintColor: black,
+      headerStyle: {
+        backgroundColor: white,
+      },
+    },
+  },
+  NewCard: {
+    screen: NewCard,
+    navigationOptions: {
+      title: 'Add Card',
+      headerTintColor: black,
+      headerStyle: {
+        backgroundColor: white,
+      },
+    },
+  },
+});
 
 export default class App extends React.Component {
-
   render() {
-
     return (
-      <View style={styles.container}>
-        <Text>
-          Hi
-        </Text>
+      <View style={{ flex: 1 }}>
+        <CardsStatusBar backgroundColor={black} barStyle='light-content' />
+        <MainNavigator />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
