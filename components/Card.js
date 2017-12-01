015265e45107549e282ea66d119249ebb1cef453
Pedro Flores <pflores@apttus.com>
2017-11-30 18:46:52 -0800

    feat: Add Quiz and Card components. The Quiz component renders Cards as it iterates through them

---
+++ components/Card.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { purple, white, black, gray, orange, blue, green } from '../utils/colors';
+
+export default class Card extends React.Component {
+
+  state = {
+    showAnswer: false,
+  };
+
+  showAnswerOrQuestion = () => {
+    this.setState({
+      showAnswer: !this.state.showAnswer,
+    });
+  };
+
+  render() {
+    const {
+      question,
+      answer,
+      incrementCorrectAnswerCount,
+      loadNextQuestion,
+    } = this.props;
+
+    return (
+      <View style={styles.container}>
+        {!this.state.showAnswer && (
+          <View>
+            <Text style={styles.questionOrAnswer}>{question}</Text>
+            <TouchableOpacity onPress={this.showAnswerOrQuestion}>
+              <Text style={styles.showAnswer}>Answer</Text>
+            </TouchableOpacity>
+            <View style={styles.separator}></View>
+          </View>
+        )}
+        {this.state.showAnswer && (
+          <View>
+            <Text style={styles.questionOrAnswer}>{answer}</Text>
+            <TouchableOpacity onPress={this.showAnswerOrQuestion}>
+              <Text style={styles.showAnswer}>Question</Text>
+            </TouchableOpacity>
+            <View style={styles.separator}></View>
+          </View>
+        )}
+        <View style={styles.buttons}>
+          <TouchableOpacity
+            style={styles.correctBtn}
+            onPress={incrementCorrectAnswerCount}
+          >
+            <Text style={styles.btnText}>Correct</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.incorrectBtn}
+            onPress={loadNextQuestion}
+          >
+            <Text style={styles.btnText}>Incorrect</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: white,
+    width: Dimensions.get('window').width,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardCount: {
+    textAlign: 'left',
+    top: 0,
+    left: 0,
+    color: gray,
+    fontSize: 20,
+    marginTop: 5,
+    marginLeft: 5,
+  },
+  questionOrAnswer: {
+    textAlign: 'center',
+    backgroundColor: white,
+    borderColor: white,
+    fontSize: 28,
+    alignSelf: 'stretch',
+  },
+  showAnswer: {
+    textAlign: 'center',
+    color: gray,
+    fontSize: 16,
+  },
+  separator: {
+    height: 80,
+  },
+  correctBtn: {
+    backgroundColor: green,
+    margin: 5,
+    padding: 10,
+    paddingLeft: 55,
+    paddingRight: 55,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 5,
+  },
+  incorrectBtn: {
+    backgroundColor: orange,
+    margin: 5,
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 5,
+  },
+  btnText: {
+    color: black,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
---
+++ components/Quiz.js
@@ -0,0 +1,207 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Card from './Card';
+import { white, black, gray, green } from '../utils/colors';
+
+const QuizFinished = ({ styles, deck, percentage, ...props }) => (
+  <View>
+    <Text style={styles.goodJobText}>Good job!</Text>
+    <Text style={styles.percentageText}>Correct answers percentage: </Text>
+    <Text style={styles.percentageResult}>{percentage}%</Text>
+    <TouchableOpacity
+      style={styles.retakeQuizBtn}
+      onPress={() => props.navigation.navigate('Quiz', { deck: deck })}
+    >
+      <Text style={styles.btnText}>Retake quiz?</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={styles.deckDetailsBtn}
+      onPress={() => props.navigation.navigate('Home', { deck: deck })}
+    >
+      <Text style={styles.btnTextWhite}>All decks</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const EmptyDeck = ({ styles, deck, percentage, ...props }) => (
+  <View>
+    <Text style={styles.noQuestionsText}>This deck is empty</Text>
+    <Text style={styles.addCartText}>Please add some questions or go back to see deck details</Text>
+    <TouchableOpacity
+      style={styles.addCardBtn}
+      onPress={() => props.navigation.navigate('NewCard', { deck: deck })}
+    >
+      <Text style={styles.btnText}>Add Card</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+export default class Quiz extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questionsIter: this.props.navigation.state.params.deck.questions.entries(),
+      correctAnswersCt: 0,
+      currentCardNum: 0,
+      currentCard: {},
+      finished: false,
+    };
+
+    // this.loadNextQuestion();
+    this.incrementCorrectAnswerCount = this.incrementCorrectAnswerCount.bind(this);
+    this.loadNextQuestion = this.loadNextQuestion.bind(this);
+  }
+
+  incrementCorrectAnswerCount = () => {
+    this.setState({
+      correctAnswersCt: this.state.correctAnswersCt + 1,
+    });
+    this.loadNextQuestion();
+  };
+
+  componentWillMount() {
+    this.loadNextQuestion();
+  }
+
+  loadNextQuestion = () => {
+    const entry = this.state.questionsIter.next();
+    if (!entry.done) {
+      this.setState({
+        currentCardNum: entry.value[0] + 1,
+        currentCard: {
+          ...entry.value[1],
+        },
+      });
+    } else {
+      // user has seen all the questions
+      this.setState({
+        finished: true,
+      });
+    }
+  };
+
+  calculatePercentage = numOfQuestions => this.state.correctAnswersCt * 100 / numOfQuestions;
+
+  render() {
+    const { deck } = this.props.navigation.state.params;
+    const { currentCard, currentCardNum, finished } = this.state;
+    const deckEmpty = deck.questions.length === 0;
+
+    return (
+      <View style={styles.container}>
+        {deckEmpty &&
+          <EmptyDeck
+            styles={styles}
+            deck={deck}
+            {...this.props}
+          />
+        }
+        {(!finished && !deckEmpty) &&
+          <View style={styles.container}>
+            <Text style={styles.cardCount}>{currentCardNum} of {deck.questions.length}</Text>
+            <Card
+              question={currentCard && currentCard.question}
+              answer={currentCard && currentCard.answer}
+              incrementCorrectAnswerCount={this.incrementCorrectAnswerCount}
+              loadNextQuestion={this.loadNextQuestion}
+            />
+          </View>
+        }
+        {(finished && !deckEmpty) &&
+          <QuizFinished
+            styles={styles}
+            percentage={this.calculatePercentage(deck.questions.length)}
+            deck={deck}
+            {...this.props}
+          />
+        }
+      </View>
+    );
+
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: white,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardCount: {
+    position: 'absolute',
+    top: 5,
+    left: 5,
+    color: gray,
+    fontSize: 20,
+  },
+  goodJobText: {
+    textAlign: 'center',
+    fontSize: 36,
+  },
+  noQuestionsText: {
+    textAlign: 'center',
+    fontSize: 38,
+    marginBottom: 20,
+  },
+  addCartText: {
+    textAlign: 'center',
+    color: gray,
+    fontSize: 24,
+    marginBottom: 40,
+  },
+  percentageText: {
+    textAlign: 'center',
+    color: gray,
+    fontSize: 24,
+  },
+  percentageResult: {
+    textAlign: 'center',
+    color: green,
+    fontSize: 38,
+    marginTop: 20,
+    marginBottom: 60,
+  },
+  deckDetailsBtn: {
+    backgroundColor: black,
+    margin: 5,
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 5,
+  },
+  retakeQuizBtn: {
+    backgroundColor: white,
+    margin: 5,
+    padding: 10,
+    paddingLeft: 60,
+    paddingRight: 60,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 5,
+    borderWidth: 1,
+  },
+  addCardBtn: {
+    backgroundColor: white,
+    margin: 5,
+    padding: 10,
+    marginLeft: 80,
+    marginRight: 80,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  btnTextWhite: {
+    color: white,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  btnText: {
+    color: black,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
